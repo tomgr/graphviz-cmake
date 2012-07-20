986573518e49ca3b067b29dc2d984076e3afe520
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2012-07-20 12:59:51 +0100

    Add a method to initialise graphviz using the preloaded symbols.

--- CMake/graphviz-static.c
+++ CMake/graphviz-static.c
@@ -16,7 +16,8 @@
 #endif
 
 #include "graphviz/gvplugin.h"
-
+#include "graphviz/gvc.h"
+ 
 #if defined(GVDLL) && !defined(ENABLE_LTDL)
 #define extern  __declspec(dllimport)
 #endif
@@ -25,10 +26,14 @@ extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
 extern gvplugin_library_t gvplugin_neato_layout_LTX_library;
 extern gvplugin_library_t gvplugin_core_LTX_library;
 
-
 lt_symlist_t lt_preloaded_symbols[] = {
     { "gvplugin_dot_layout_LTX_library", (void*)(&gvplugin_dot_layout_LTX_library) },
     { "gvplugin_neato_layout_LTX_library", (void*)(&gvplugin_neato_layout_LTX_library) },
     { "gvplugin_core_LTX_library", (void*)(&gvplugin_core_LTX_library) },
     { 0, 0 }
 };
+
+GVC_t* gvContextWithStaticPlugins()
+{
+    return gvContextPlugins(lt_preloaded_symbols, 0);
+}
